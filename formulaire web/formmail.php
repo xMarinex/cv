5142777b7898aa5e4ab7c72e04f7ab7ac2b1fe25
xMarinex <39373096+xMarinex@users.noreply.github.com>
2018-06-03 23:03:41 +0200

    Delete formmail.php

--- formulaire web/formmail.php
+++
@@ -1,1 +0,0 @@
-<?php $TO = "marinegoa.pro@gmail.com"; $h = "From: " . $TO; $message = ""; while (list($key, $val) = each($HTTP_POST_VARS)) { $message .= "$key : $val\n"; } mail($TO, $subject, $message, $h); Header("Location: http://localhost:8080/formulaire web/message_envoye.html"); ?>
